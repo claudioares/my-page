5bc53a7cc82b9690a3dfaa2c13705512ef75e720
claudioares <claudiojasoares.dev@gmail.com>
2024-04-26 21:53:40 -0300

    fix: Adicionando pasta de backend

---
+++ backend/my-page-API-contacts/src/app.ts
@@ -0,0 +1,31 @@
+import fastify, { FastifyInstance } from "fastify"
+import { CreteEvents } from "./routes/create.messege.client";
+import { GetMesseges } from "./routes/getMesseges.client";
+import { getMessegeId } from "./routes/getMessege.client.id";
+import cors from "@fastify/cors";
+
+
+export class App {
+    private app: FastifyInstance;
+    constructor() {
+        this.app = fastify()
+    }
+
+
+    listen(){
+        this.app.listen({
+            host: '0.0.0.0',
+            port: process.env.PORT ? Number(process.env.PORT) : 3333,
+        }).then(()=>console.log("HTTP Server running..."));
+    };
+
+    register(){
+        this.app.register(cors, {
+            origin: "*",
+            methods: ['POST']
+        })
+        this.app.register(CreteEvents);
+        this.app.register(GetMesseges);
+        this.app.register(getMessegeId);
+    }
+}
